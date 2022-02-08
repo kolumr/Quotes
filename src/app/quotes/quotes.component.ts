2175be0efe4ab9f2960c7b8688a33ec34e2e2f9b
Ronald Kolum <kolumronnie@gmail.com>
2022-02-08 12:25:51 +0300

    Added quote object

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quotes',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-
+  quotes:Quote[] = [
+    new Quote('Even the rich cry','Anonymous','James', new Date(2019,6,9),5,0),
+    new Quote('It is never too late to start. Start now, start strong, but Just start.','James Brown','Brian',new Date(),15,7),      
+    new Quote('Courage is built by countering adversity. Seek moments of courage','Shaolin Master','Andrew',new Date(2019,6,9),10,5),
+    new Quote('The world is seeking for men, men who cannot be bought or sold','Ellen White','Brian',new Date(),15,7),
+  ]
   constructor() { }
 
   ngOnInit(): void {
