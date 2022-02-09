bb917d8d72301cf2f051cd4fe94b6ae9f04ee5b3
Ronald Kolum <kolumronnie@gmail.com>
2022-02-09 14:13:19 +0300

    Moved author to the description

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Quote } from '@angular/compiler';
 @Component({
   selector: 'app-details',
-  template: `<p>Posted by: {{quote[id].poster}}<p>`,
+  templateUrl: 'details.component.html',
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
