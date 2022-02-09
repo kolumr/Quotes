d7c8181d8cc2d62b07883be88062083708c22d47
Ronald Kolum <kolumronnie@gmail.com>
2022-02-09 12:26:33 +0300

    added property binding to display details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Quote } from '@angular/compiler';
 @Component({
   selector: 'app-details',
-  templateUrl: './details.component.html',
+  template: `<p>Posted by: {{quote[id].poster}}<p>`,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
-  @Input() quote:any = Quote;
+  @Input() quote:any;
+  @Input() id:any;
   constructor() { }
 
   ngOnInit(): void {
